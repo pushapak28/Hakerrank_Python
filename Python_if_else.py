781fcdeffb0ed7b6754b79984e0157b014e0263e
Pushapak Kumar <pp281997@gmail.com>
2022-06-25 15:47:07 +0530

    hakerank practice

---
+++ Python_if_else.py
@@ -0,0 +1,63 @@
+# Task
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
+
+# A single line containing a positive integer, .
+
+# Constraints
+
+# Output Format
+
+# Print Weird if the number is weird. Otherwise, print Not Weird.
+
+# Sample Input 0
+
+# 3
+# Sample Output 0
+
+# Weird
+# Explanation 0
+
+
+#  is odd and odd numbers are weird, so print Weird.
+
+# Sample Input 1
+
+# 24
+# Sample Output 1
+
+# Not Weird
+# Explanation 1
+
+
+#  and  is even, so it is not weird.
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n%2==0:
+    if n in range(2,6):
+        print("Not Weird")
+    elif n in range(6,20):
+        print("Weird")
+    elif n>20:
+        print("Not Weird")
+        
+else:
+    print("Weird")
+    
