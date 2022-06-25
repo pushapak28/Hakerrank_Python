b5e91e18bccb9e4c1d6d5df19bd7a8dc93d8bd82
Pushapak Kumar <pp281997@gmail.com>
2022-06-25 15:49:07 +0530

    Update Python_if_else.py

--- Python_if_else.py
+++ Python_if_else.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
 if n%2==0:
     if n in range(2,6):
         print("Not Weird")
-    elif n in range(6,20):
+    elif n in range(6,21):
         print("Weird")
     elif n>20:
         print("Not Weird")
