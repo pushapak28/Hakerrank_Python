251c339de30e9976c97de119ec4dfcc5c2e34672
Pushapak Kumar <pp281997@gmail.com>
2022-07-09 18:48:20 +0530

    Create String_Substring.py

---
+++ String_Substring.py
@@ -0,0 +1,13 @@
+def count_substring(string, sub_string):
+    res = sum(1 for i in range(len(string)) 
+         if string.startswith(sub_string, i))
+  
+    
+    return res
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
