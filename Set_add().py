5c7b2d552ae22411e2e364410d52202f45483402
Pushapak Kumar <pp281997@gmail.com>
2022-07-30 12:11:11 +0530

    Create Set_add().py

---
+++ Set_add().py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+int1 = int(input())
+string1 = set()
+for i in range (int1):
+    string1.add(str(input()))
+print(len(string1))
