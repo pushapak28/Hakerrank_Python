2f1d1e7991fd2c49e4db3cfe98ed57131cf1188e
Pushapak Kumar <pp281997@gmail.com>
2022-08-04 11:56:03 +0530

    Create Set_union.py

---
+++ Set_union.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = input()
+set_a = set(map(int, input().split()))
+b = input()
+set_b = set(map(int, input().split()))
+print(len(set_a.union(set_b)))
